0c1a2f07739ae09e93b5df83a3a04c9ea4ae72dd
DavidVydra <davidthomasvydra@gmail.com>
2023-08-21 16:56:44 -0300

    Update Ejercicio1.aspx.cs
    elimine un parentesis de mas

--- tp2-prog3/Ejercicio1.aspx.cs
+++ tp2-prog3/Ejercicio1.aspx.cs
@@ -25,18 +25,20 @@ namespace tp2_prog3
                 Lbtabla.ForeColor = System.Drawing.Color.Red;
                 return;
             }
-            if (producto1.ToLower() == producto2.ToLower())
+            if (producto1.ToLower() == producto2.ToLower()) {
                 Lbtabla.Text = "Error: Los nombres de productos no pueden ser iguales";
                 Lbtabla.ForeColor = System.Drawing.Color.Red;
                 return;
+            }
 
             int cantidad1 = int.Parse(TbCantProd1.Text.Trim());
             int cantidad2 = int.Parse(TbCantProd2.Text.Trim());
 
-            if (cantidad1 <= 0 || cantidad2 <= 0)
-             Lbtabla.Text = "Error: La cantidad de productos no puede ser negativo o cero");
-             Lbtabla.ForeColor = System.Drawing.Color.Red;
-             return;
+            if (cantidad1 <= 0 || cantidad2 <= 0) {
+                Lbtabla.Text = "Error: La cantidad de productos no puede ser negativo o cero";
+                Lbtabla.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
 
             string tabla = "<table border='1'>";
             tabla += "<tr><th>Producto</th><th>Cantidad</th></tr>";
