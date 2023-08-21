62d06078cdb86a78ce9997d0c9d286542fd32d42
Alejo <alejoperezherrero@gmail.com>
2023-08-21 15:20:24 -0300

    primer aporte ej 1

---
+++ tp2-prog3/Ejercicio1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="tp2_prog3.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LblProd1" runat="server" Text="Ingrese nombre del producto:"></asp:Label>
+            <asp:TextBox ID="TbProd1" runat="server" style="margin-left: 5px; margin-right: 10px"></asp:TextBox>
+            <asp:Label ID="LblCantProd1" runat="server" Text="Cantidad:"></asp:Label>
+            <asp:TextBox ID="TbCantProd1" runat="server" style="margin-left: 5px; margin-right: 0px;"></asp:TextBox>
+            <br />
+            <asp:Label ID="LblProd2" runat="server" Text="Ingrese nombre del producto:"></asp:Label>
+            <asp:TextBox ID="TbProd2" runat="server" style="margin-left: 5px; margin-right: 10px; margin-top: 10px"></asp:TextBox>
+            <asp:Label ID="LblCantProd2" runat="server" Text="Cantidad:"></asp:Label>
+            <asp:TextBox ID="TbCantProd2" runat="server" style="margin-left: 5px; margin-right: 0px;"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ tp2-prog3/Ejercicio1.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tp2_prog3
+{
+
+
+    public partial class Ejercicio1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control LblProd1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblProd1;
+
+        /// <summary>
+        /// Control TbProd1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbProd1;
+
+        /// <summary>
+        /// Control LblCantProd1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblCantProd1;
+
+        /// <summary>
+        /// Control TbCantProd1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbCantProd1;
+
+        /// <summary>
+        /// Control LblProd2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblProd2;
+
+        /// <summary>
+        /// Control TbProd2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbProd2;
+
+        /// <summary>
+        /// Control LblCantProd2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblCantProd2;
+
+        /// <summary>
+        /// Control TbCantProd2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbCantProd2;
+    }
+}
